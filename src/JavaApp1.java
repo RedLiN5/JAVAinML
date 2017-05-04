ac6eea09a09b8db621f15c58dc42e24cdda6fe71
Leslie_Yan <leslieyan0621@gmail.com>
2017-05-04 22:14:13 +0800

    Print current working dir

---
+++ src/JavaApp1.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Leslie on 5/4/17.
+ */
+public class JavaApp1 {
+    public static void main(String[] args){
+        System.out.println("Current working directory:" +
+                System.getProperty("user.dir"));
+    }
+}
