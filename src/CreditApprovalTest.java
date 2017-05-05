250b2b8965affafc8eedd6ca2da9dc69b9b95306
Leslie_Yan <leslieyan0621@gmail.com>
2017-05-05 17:32:38 +0800

    Split data into training and testing parts

--- src/CreditApprovalTest.java
+++ src/CreditApprovalTest.java
@@ -1,10 +1,16 @@
 import weka.classifiers.Classifier;
 import weka.classifiers.Evaluation;
+import weka.classifiers.bayes.NaiveBayes;
+import weka.classifiers.functions.Logistic;
+import weka.classifiers.meta.AdaBoostM1;
+import weka.classifiers.pmml.consumer.SupportVectorMachineModel;
 import weka.core.Instances;
+import weka.classifiers.trees.J48;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.ArrayList;
 
 /**
  * Created by Leslie on 5/5/17.
@@ -36,18 +42,42 @@ public class CreditApprovalTest {
 
 
     public static void main(String[] args) throws Exception {
-        BufferedReader data = readDataFile("data/credit_approval_clean.csv");
+        BufferedReader dataFile = readDataFile("data/credit_approval_clean.csv");
         String line = null;
         int i = 0;
         try {
-            while(((line = data.readLine()) != null) && i <10) {
+            while(((line = dataFile.readLine()) != null) && i <10) {
                 System.out.println(line);
                 i++;
-
             }
         }
         finally {
-            data.close();
+            dataFile.close();
+        }
+
+        Instances data = new Instances(dataFile);
+        data.setClassIndex(data.numAttributes() - 1);
+
+        int trainSize = (int) Math.round(data.numInstances() * 0.75);
+        int testSize = data.numInstances() - trainSize;
+
+        Instances train = new Instances(data, 0, trainSize);
+        Instances test = new Instances(data, trainSize, testSize);
+
+        Classifier[] models = {
+                new J48(),
+                new NaiveBayes(),
+                new Logistic(),
+                new AdaBoostM1()
+        };
+
+        for (int j = 0; j < models.length; j++) {
+            ArrayList predictions = new ArrayList();
+
+            Evaluation validation = classify(models[j], train, test);
+
+            predictions.add(validation.predictions());
+
         }
     }
 }
